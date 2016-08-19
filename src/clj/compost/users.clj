2489033d5e6ba3c062a4509758fcc814d75c5ce1
John Mandalakas <john@mandalakas.com>
2016-08-18 20:51:41 -0500

    Add user validation infor

--- src/clj/compost/users.clj
+++ src/clj/compost/users.clj
@@ -11,8 +11,22 @@
   (select user
           (where {:email email})))
 
+;; {
+;;   "name": "tester mc test",
+;;   "email": "test1@test.com",
+;;   "password": "test123",
+;;   "password_confirmation": "test123",
+;;   "participant_type": "resident",
+;;   "questionnaire": []
+;; }
+
+(defn password-present? [user-data]
+  (println (:password user-data))
+  (println (:password_confirmation user-data)))
+
 (defn new [user-data]
+  (password-present? user-data)
   (insert user
           (values [(merge {:created_at (to-sql-time (now))
                            :updated_at (to-sql-time (now))}
-                           user-data)])))
+                          user-data)])))
